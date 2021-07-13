675582caa2c89300b6bc869cc39398a9c5ed8d02
Akhil S Pillai <akhil@revemarketing.com>
2021-07-13 18:34:48 +0530

    Startup express code

---
+++ packages/backend/src/app.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import cors from "cors";
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (request, response) => {
+  response.send("Hello world!");
+});
+
+export default app;
