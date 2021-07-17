da2de4a68aba6fcc2feb91c4573cfa27a0f62a3e
Akhil S Pillai <akhil@revemarketing.com>
2021-07-17 22:29:29 +0530

    Adds deploy script for CI

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+# !/bin/bash
+
+cd ~/simple-portfolio
+
+# pull from the branch
+git pull origin master
+
+# start the containers
+docker-compose up -d
+docker-compose -f nginx-proxy-compose.yaml up -d
