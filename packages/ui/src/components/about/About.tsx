8f34f409aa4fde8cf4a9406be18aa0edeb392c61
Akhil S Pillai <akhil@revemarketing.com>
2021-07-17 23:03:35 +0530

    Fixes typos

--- packages/ui/src/components/about/About.tsx
+++ packages/ui/src/components/about/About.tsx
@@ -4,12 +4,7 @@ import { ReactElement } from "react";
 import "./About.css";
 
 export const ABOUT =
-  "I have over 10 years of industry experience in developing commercial applications and have been a part of exceptional " +
-  "design and development projects. In previous roles, I worked on cutting edge technologies like blockchain, implemented " +
-  "scalable solutions, built mobile as well as web applications and executed innovative ideas. I have experiance in application " +
-  "deployment using cloud technologies such as AWS, containerization using docker and their orchestration using kubernetes as well. " +
-  "I have always been a quick learner, jumping right into technology stacks as and when required while delivering high " +
-  "quality software.";
+  "I have over ten years of industry experience in developing commercial applications and have been a part of exceptional design and development projects. In previous roles, I worked on cutting-edge technologies like blockchain, implemented scalable solutions, built mobile as well as web applications, and executed innovative ideas. I have experience in application deployment using cloud technologies such as AWS, containerization using docker, and their orchestration using Kubernetes. I have always been a quick learner, jumping right into technology stacks as and when required while delivering high-quality software.";
 
 export default function About(): ReactElement {
   return (
